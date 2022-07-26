932196d80a79e379099d7604de342563e4b39a26
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2022-07-25 23:51:09 -0300

    Alternando os estágios do game

--- src/App.js
+++ src/App.js
@@ -13,20 +13,35 @@ import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
-  {id: 1, name: 'start'},
-  {id: 2, name: 'game'},
-  {id: 3, name: 'end'},
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
 ]
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  // Start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // Restart the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen/>}
-      {gameStage === 'game' && <Game/>}
-      {gameStage === 'end' && <GameOver/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }
