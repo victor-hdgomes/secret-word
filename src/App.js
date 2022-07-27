ad6f27c9666c8c82b2974d4b18d5fb13b65ec64f
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2022-07-27 10:30:12 -0300

    Escolhendo categoria e palavra aleatoria

--- src/App.js
+++ src/App.js
@@ -22,8 +22,35 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    // Pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    // Pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { category, word }
+  }
+
   // Start the secret word game
   const startGame = () => {
+    // Pick word and pick category
+    const { category, word } = pickWordAndCategory()
+
+    // Create an array of letters
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map((L) => L.toLowerCase())
+
+    // Fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+
     setGameStage(stages[1].name)
   }
 
@@ -40,8 +67,8 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
